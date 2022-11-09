c8d1d2490095ef22d3fdbaa97a3d39ef84417312
shariful4345 <mdsajeeb4345@gmail.com>
2022-11-09 08:29:00 +0600

    assignment-11 siver project all data commit

---
+++ index.js
@@ -0,0 +1,63 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port =process.env.PORT || 5000;
+
+require('dotenv').config();
+
+// middle ware
+
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.y8s6gcn.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        const travelCollection = client.db('travelGuidline').collection('tourServices')
+
+        app.get('/tourServices', async(req,res)=>{
+           const query = {}
+           const cursor =travelCollection.find(query)
+           const tourServices = await cursor.limit(3).toArray();
+           res.send(tourServices)
+       })
+
+        app.get('/tourAllCard', async (req, res) => {
+            const query = {}
+            const cursor = travelCollection.find(query)
+            const tourServices = await cursor.toArray();
+            res.send(tourServices)
+
+        })
+
+        // app.delete('/click/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     const query = { _id: ObjectId(id) };
+        //     const result = await orderCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+
+    }
+   
+    finally{
+
+    }
+}
+run().catch(err=>console.log(err));
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send('travels and tour server is running')
+});
+
+app.listen(port, ()=>{
+    console.log(`travels and tour server running on ${port}`);
+});
